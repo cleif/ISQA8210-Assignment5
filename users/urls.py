54aa85037cf58aa9ef24fe3a9aa43d935f00542c
Beartato327 <rcapurro@unomaha.edu>
2020-10-25 19:40:41 -0500

    Updating user views to edit

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import SignUpView
-
+from .views import (
+    SignUpView,
+    UserUpdateView,
+    UserDeleteView,
+)
+from . import views
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('<uuid:pk>/edit/', UserUpdateView.as_view(), name='user_edit'),
+    path('<uuid:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+    path('account/', views.accountView, name='account'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic.edit import UpdateView, DeleteView
+from .models import CustomUser
+from django.views import generic, View
+from django.shortcuts import render
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +11,17 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class UserUpdateView(UpdateView):
+    model = CustomUser
+    fields = ('first_name', 'last_name')
+    template_name = 'user_edit.html'
+
+class UserDeleteView(DeleteView):
+    model = CustomUser
+    template_name = 'user_delete.html'
+    success_url = reverse_lazy('')
+
+def accountView(request):
+    #user = request.user
+    return render(request, 'account.html')
